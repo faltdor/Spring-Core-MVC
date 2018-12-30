61322b2e7fa463e07025e5de423e1d520d954523
Wilton Cardona <faltdor@gmail.com>
2018-12-30 13:47:30 -0500

    Create entities

--- src/main/java/com/faltdor/springmvc/domain/Product.java
+++ src/main/java/com/faltdor/springmvc/domain/Product.java
@@ -1,10 +1,16 @@
 package com.faltdor.springmvc.domain;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 
+@Entity
 public class Product implements DomainObject{
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Version
+    private Integer version;
     private String description;
     private BigDecimal price;
     private String imageUrl;
@@ -16,6 +22,14 @@ public class Product implements DomainObject{
     @Override
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
     }
 
     public String getDescription() {
